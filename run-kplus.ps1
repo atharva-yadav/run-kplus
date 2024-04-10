07513ffc2c39037f4509579ec73a524bd887186b
Atharva Yadav <78128129+atharva-yadav@users.noreply.github.com>
2024-04-10 11:53:10 +0530

    Update run-kplus.ps1

--- run-kplus.ps1
+++ run-kplus.ps1
@@ -1,27 +1,40 @@
-# script execution policy
-Set-ExecutionPolicy RemoteSigned -Force
+$choice = Read-Host "Do you want to run all 4 files? (Y/N)"
 
-Write-Host "KEP+ Setup Starting..."
+if($choice -eq "Y") {
+    Write-Host "Running all 4 files only"
+    Write-Host "KEP+ Setup Starting..."
 
-Write-Host "Running atlasCleanHelm.bat"
-& .\atlasCleanHelm.bat
+    Write-Host "Running atlasCleanHelm.bat"
+    & .\atlasCleanHelm.bat
 
-Write-Host "Running atlasStartHelm.bat"
-& .\atlasStartHelm.bat
+    Write-Host "Running atlasStartHelm.bat"
+    & .\atlasStartHelm.bat
 
-Write-Host "Changing Directory to Authorized/local-deployment"
-Set-Location ..\..\Authorized\local-deployment\
+    Write-Host "Changing Directory to Authorized/local-deployment"
+    Set-Location ..\..\Authorized\local-deployment\
 
-Write-Host "Running build.ps1 with arguments"
-& .\build.ps1 Seed --HubName unified-edge.azure-devices.net --HubConnectionString "HostName=unified-edge.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=gPZeFWB1wZBkNPTgbuYdurNkLeWKbtfeyW018="
+    Write-Host "Running build.ps1 with arguments"
+    & .\build.ps1 Seed --HubName unified-edge.azure-devices.net --HubConnectionString "HostName=unified-edge.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=gPZeFWB1wZBkNPTgbuYdurNkLeWKbtfeyW018="
 
-Write-Host "Changing Directory to ptc-solution-ui/qa-setup"
-Set-Location ..\..\ptc-solution-ui\qa-setup\
+    Write-Host "Changing Directory to ptc-solution-ui/qa-setup"
+    Set-Location ..\..\ptc-solution-ui\qa-setup\
 
-Write-Host "Running startAtlasMock.bat"
-& .\startAtlasMock.bat
+    Write-Host "Running startAtlasMock.bat"
+    & .\startAtlasMock.bat
 
-Write-Host "Running startEs.bat"
-& .\startEs.bat
+    Write-Host "Running startEs.bat"
+    & .\startEs.bat
 
-Write-Host "Setup Running"
+    Write-Host "Setup Running"
+} elseif($choice -eq "N"){
+    Write-Host "Running 2 files only"
+    Write-Host "Running startAtlasMock.bat"
+    & .\startAtlasMock.bat
+
+    Write-Host "Running startEs.bat"
+    & .\startEs.bat
+
+    Write-Host "Setup Running"
+} else {
+    Write-Host "Invalid choice. Exiting.. "
+}
